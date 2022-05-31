3ea240ba13d79c074eb0aff3267aebb74432aae0
ywanzhou <ywanzhou@foxmail.com>
2022-05-31 22:41:27 +0800

    模块化

---
+++ es新特性/src/es2015/08_模块化/01.js
@@ -0,0 +1,11 @@
+// import a from 'm' // 导入模块 m 中的默认导出，将其命名为 a
+// import a, { b } from 'm' // 导入模块 m 中的默认导出以及单独导入成员 b
+// import * as A from 'm' // 导入模块中的所有成员
+// import 'm' // 执行 m 模块
+
+// export const b = 1 // 单独导出
+// export default b // 默认导出
+// export { b } // 按需导出
+// export { b as bb } // 改名导出
+
+// export { b } from 'm' // 导入模块 m 中的成员 b 并导出
