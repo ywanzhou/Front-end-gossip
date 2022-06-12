607d90bbe185f70231d39cb52570391669e44c19
ywanzhou <ywanzhou@foxmail.com>
2022-06-12 23:31:46 +0800

    class

---
+++ es新特性/src/es2022/01_class.js
@@ -0,0 +1,15 @@
+// class C {
+//   myName = '一碗周'
+// }
+// /* 两者是一致的 */
+// class C {
+//   constructor() {
+//     myName = '一碗周'
+//   }
+// }
+
+// class C {
+//   #myName = '一碗周'
+// }
+// const c = new C()
+// console.log(#myName) //  Private field '#myName' must be declared in an enclosing class
