9b40075a0754ca097dba81ae34255b11daef9309
ywanzhou <ywanzhou@foxmail.com>
2022-06-12 18:03:47 +0800

    异步迭代

---
+++ es新特性/src/es2018/01_异步迭代.js
@@ -0,0 +1,24 @@
+var asyncIterable = {
+  [Symbol.asyncIterator]() {
+    return {
+      i: 0,
+      next() {
+        if (this.i < 3) {
+          return Promise.resolve({ value: this.i++, done: false })
+        }
+
+        return Promise.resolve({ done: true })
+      },
+    }
+  },
+}
+
+;(async function () {
+  for await (num of asyncIterable) {
+    console.log(num)
+  }
+})()
+
+// 0
+// 1
+// 2
